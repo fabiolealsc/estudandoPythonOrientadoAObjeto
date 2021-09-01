018ae8559a1bd5730ca377643e005fe81514d17b
Fabio Leal Schmitz <fabio.leal.sc@gmail.com>
2021-09-01 02:14:35 -0300

    Adicionando código!
    Foi adicionado funções a nosso objeto pessoa.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from pessoa import Pessoa
 
-p1 = Pessoa()
-p2 = Pessoa()
+p1 = Pessoa('Pedro', 25)
 
-p1.nome = 'João'
-p2.nome = 'Maria'
+p1.comer('maçã')
+p1.comer('maçã')
+p1.parar_comer()
+p1.parar_comer()
+p1.falar('casa')
+p1.falar('casa')
+p1.comer('pera')
+p1.parar_falar()
+p1.comer('uva')
 
-p1.falar()
-p2.falar()
+
